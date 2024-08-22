82a959b442be3ef7c87100814e1849e79094f879
Paul Maddern <pobster@25159.no-reply.drupal.org>
2024-08-22 22:43:11 +0100

    Skoolkit Control File Generator; Fix "IX" command.

--- generator/skoolkit_controlfile_generator.py
+++ generator/skoolkit_controlfile_generator.py
@@ -180,6 +180,10 @@ class Disassembler:
                 self.lines.append('  ${:X},$03 Write {} to *#REGix+#N${:02X}.'.format(self.pc, IX_LOAD[cmd],
                                                                               self.snapshot[self.pc + 0x02]))
                 self._pc += 0x03
+            elif cmd in [0x2A]:
+                value = self.get_address(self.pc + 0x02)
+                self.lines.append('  ${:X},$04 {}=*#R${:X}.'.format(self.pc, IX_LOAD[cmd], value))
+                self._pc += 0x04
             else:
                 value = self.get_address(self.pc + 0x02)
                 self.lines.append('  ${:X},$04 {}=#R${:X}.'.format(self.pc, IX_LOAD[cmd], value))
